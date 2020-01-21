34f9d31a30da4e002a9d08caa8a3a0beb180c5ad
ilkercolakoglu <colakogluilker@gmail.com>
2020-01-22 00:09:52 +0300

    count set to response header

--- src/main/java/com/turkcell/poc/customerupdater/controller/CustomerController.java
+++ src/main/java/com/turkcell/poc/customerupdater/controller/CustomerController.java
@@ -4,9 +4,11 @@ import com.turkcell.poc.customerupdater.dto.CustomerDTO;
 import com.turkcell.poc.customerupdater.dto.Response;
 import com.turkcell.poc.customerupdater.service.CustomerService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.protocol.types.Field;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 import static com.turkcell.poc.customerupdater.util.CustomerConstants.CUSTOMER_FAILED;
@@ -44,9 +46,11 @@ public class CustomerController {
 
 
     @GetMapping
-    public Response createCustomer(@RequestParam Integer from,@RequestParam Integer size) {
+    public Response getCustomers(@RequestParam Integer from, @RequestParam Integer size, HttpServletResponse response) {
         try {
             List<CustomerDTO> customerDTOList = customerService.getCustomersByRange(from, size);
+            long total = customerService.count();
+            response.addHeader("total", String.valueOf(total));
             return Response.builder()
                     .httpStatus(HttpStatus.OK.value())
                     .data(customerDTOList)
